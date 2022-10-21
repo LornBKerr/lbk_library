fccc495603e9e6737f437b6f3f2e08988eff7a2c
LornBKerr <lornburtkerr@gmail.com>
2022-10-21 10:51:56 -0400

    Add qt sub-package

---
+++ src/lbk_library/qt/__init__.py
@@ -0,0 +1,10 @@
+"""
+Provide common dialog elements for all projects
+
+This module contains the following classes
+    FocusComboBox  -Extend the standard QComboBox to emit the 
+        'activate' signal when the focus is lost.
+    Dialog - Base class for the vaious dialog forms, extending the
+        QDialog class.
+"""
+
