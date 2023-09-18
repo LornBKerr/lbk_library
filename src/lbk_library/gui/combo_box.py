1791ae640d06d91736b0136010663a78948a9cbe
LornBKerr <lornburtkerr@gmail.com>
2023-09-18 17:40:30 -0400

    Added error handling to the combo box; passed focusOutEvent to super class for further handling as necessary.

--- src/lbk_library/gui/combo_box.py
+++ src/lbk_library/gui/combo_box.py
@@ -11,18 +11,20 @@ from PyQt5.QtGui import QFocusEvent
 from PyQt5.QtWidgets import QComboBox, QWidget
 
 
-class FocusComboBox(QComboBox):
+class ComboBox(QComboBox):
     """Initialize the Focus Combo Box."""
 
     def __init__(self, parent: QWidget = None) -> None:
         """
-        Initialize the Focus Combo Box.
+        Initialize the Combo Box.
 
         Parameters:
             parent(QWidget) the parent widget holding the QComboBo, may be
                 empty.
         """
         super().__init__(parent)
+        self.error_frame = None
+        self._error = False
 
     def focusOutEvent(self, evt: QFocusEvent) -> None:
         """
@@ -34,3 +36,17 @@ class FocusComboBox(QComboBox):
         self.activated.emit(self.currentIndex())
         super().focusOutEvent(evt)
 
+    @pyqtProperty(bool)
+    def error(self) -> bool:
+        """Get the error status."""
+        return self._error
+
+    @error.setter
+    def error(self, value: bool) -> None:
+        """Set the error status."""
+        self._error = value
+        self.error_frame.error = value
+
+    def set_frame(self, frame: QFrame = None) -> None:
+        """Set the related ErrorFrame."""
+        self.error_frame = frame
