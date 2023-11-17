80bfa5fff0cc11137e3f3804856e447d33f455ce
LornBKerr <lornburtkerr@gmail.com>
2023-11-17 11:25:47 -0500

    Updated comments to reflect added classes.

--- src/lbk_library/__init__.py
+++ src/lbk_library/__init__.py
@@ -10,9 +10,18 @@ This package contains the following classes:
     IniFileParser  Read and Write *.ini Files
     Validate       Support validation of values going into the database.
 
-    gui.Dialog          Base class for various dialogs used in projects.
-    gui.FocusComboBox   Extend QComboBox to emit the activate signal when
-                        the focus is lost.
+    gui.ComboBox extends QComboBox - Emits the 'activate' signal when
+        the focus is lost.
+     gui.Dialog extends QDialog - Base class for various dialogs used in projects.
+     gui.ErrorFrame extends QFrame - Provides a red border around a dialog
+        member to indicate an error.
+     gui.LineEdit extends QLineEdit - Emits the 'editingFinished' signal when
+        the focus is lost.
+     gui.RowState -Enumerates the various states a table row may have.
+     gui.TableComboBox - Encapsules a ComboBox and an ErrorFrame into a
+        widget to place in a QtableWidget row.
+     gui.TableLineEdit - Encapsules a LineEdit and an ErrorFrame into a
+        widget to place in a QtableWidget row.
 
 File:       __init__.py
 Author:     Lorn B Kerr
@@ -23,6 +32,14 @@ License:    see License
 from .dbal import Dbal
 from .element import Element
 from .element_set import ElementSet
-from .gui import ComboBox, Dialog, ErrorFrame, LineEdit, RowState, TableLineEdit, TableComboBox
+from .gui import (
+    ComboBox,
+    Dialog,
+    ErrorFrame,
+    LineEdit,
+    RowState,
+    TableComboBox,
+    TableLineEdit,
+)
 from .ini_file_parser import IniFileParser
 from .validate import Validate
