b16a9ed97990dc9d1e688967c0b166035691e554
LornBKerr <lornburtkerr@gmail.com>
2024-05-15 12:43:07 -0400

    Updated to use the DataFile instead of Dbal.

--- tests/test_09_dialog.py
+++ tests/test_09_dialog.py
@@ -7,68 +7,53 @@ Copyright:  (c) 2023 Lorn B Kerr
 License:    MIT, see file License
 """
 
-import os
-import sys
-
-from PyQt5.QtWidgets import QComboBox, QDialog, QMainWindow, QMessageBox
-from pytestqt import qtbot
-
-# from pytestqt.qt_compat import qt_api
-
-
-src_path = os.path.join(os.path.realpath("."), "src")
-if src_path not in sys.path:
-    sys.path.append(src_path)
-
+from PyQt5.QtWidgets import QDialog, QMainWindow, QMessageBox  # QComboBox,
 from test_setup import (
     A_Form,
     better_element,
-    db_close,
-    db_create,
-    db_open,
     element_values,
     new_element,
     save_something,
 )
 
-from lbk_library import Dbal, Element
+from lbk_library import DataFile, Element
 from lbk_library.gui import Dialog
+from lbk_library.testing_support import datafile_close
+
+
+def base_setup(qtbot):
+    datafile = DataFile()
+    main = QMainWindow()
+    dialog = Dialog(main, datafile, Dialog.VIEW_ELEMENT)
+    qtbot.addWidget(main)
+    return (dialog, main, datafile)
 
 
 def test_06_01_class_type(qtbot):
-    dbref = Dbal()
-    main = QMainWindow()
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
-    qtbot.addWidget(main)
+    dialog, main, datafile = base_setup(qtbot)
     assert isinstance(dialog, Dialog)
     assert isinstance(dialog, QDialog)
+    datafile_close(datafile)
 
 
-def test_06_02_get_dbref(qtbot):
-    dbref = Dbal()
-    main = QMainWindow()
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
-    qtbot.addWidget(main)
-    assert dialog.get_dbref() == dbref
+def test_06_02_get_datafile(qtbot):
+    dialog, main, datafile = base_setup(qtbot)
+    assert dialog.get_datafile() == datafile
 
 
 def test_06_03_get_set_element(qtbot):
-    dbref = Dbal()
-    main = QMainWindow()
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
+    dialog, main, datafile = base_setup(qtbot)
     assert dialog.get_element() is None
-    dialog.set_element(new_element(dbref))
+    dialog.set_element(new_element(datafile))
     qtbot.addWidget(main)
     assert isinstance(dialog.get_element(), Element)
-    dialog.set_element(better_element(dbref))
+    dialog.set_element(better_element(datafile))
     assert isinstance(dialog.get_element(), Element)
+    datafile_close(datafile)
 
 
 def test_06_04_msg_info_close(qtbot):
-    dbref = Dbal()
-    main = QMainWindow()
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
-    qtbot.addWidget(main)
+    dialog, main, datafile = base_setup(qtbot)
     msg = "Testing Information Box"
     msg_box = dialog.message_information_close(msg)
     assert msg_box.icon() == QMessageBox.Icon.Information
@@ -78,13 +63,11 @@ def test_06_04_msg_info_close(qtbot):
         msg_box.standardButtons()
         == QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No
     )
+    datafile_close(datafile)
 
 
 def test_06_05_msg_quest_changed_close(qtbot):
-    dbref = Dbal()
-    main = QMainWindow()
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
-    qtbot.addWidget(main)
+    dialog, main, datafile = base_setup(qtbot)
     changed_name = "description"
     msg = "Do you want to save the current changes before closing form?"
     msg_box = dialog.message_question_changed_close(changed_name)
@@ -97,13 +80,11 @@ def test_06_05_msg_quest_changed_close(qtbot):
         | QMessageBox.StandardButton.No
         | QMessageBox.StandardButton.Cancel
     )
+    datafile_close(datafile)
 
 
-def test_06_06_msg_quest_changed(qtbot):
-    dbref = Dbal()
-    main = QMainWindow()
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
-    qtbot.addWidget(main)
+def test_06_06_msg_quest_change(qtbot):
+    dialog, main, datafile = base_setup(qtbot)
     changed_name = "description"
     msg = "Do you want to save the current changes before reloading form?"
     msg_box = dialog.message_question_changed(changed_name)
@@ -119,10 +100,7 @@ def test_06_06_msg_quest_changed(qtbot):
 
 
 def test_06_07_msg_quest_no_changes(qtbot):
-    dbref = Dbal()
-    main = QMainWindow()
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
-    qtbot.addWidget(main)
+    dialog, main, datafile = base_setup(qtbot)
     title = "Form Entries Have Not Changed"
     msg = "Nothing has changed, so nothing to save"
     msg_box = dialog.message_question_no_changes()
@@ -130,13 +108,11 @@ def test_06_07_msg_quest_no_changes(qtbot):
     assert msg_box.windowTitle() == title
     assert msg_box.text() == msg
     assert msg_box.standardButtons() == QMessageBox.StandardButton.Ok
+    datafile_close(datafile)
 
 
-def test_06_08_msg_warning_selection(qtbot):
-    dbref = Dbal()
-    main = QMainWindow()
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
-    qtbot.addWidget(main)
+def test_06_08_msg_warning_selectio(qtbot):
+    dialog, main, datafile = base_setup(qtbot)
     name = "Source"
     action = "Save"
     msg = "Nothing to "
@@ -145,13 +121,11 @@ def test_06_08_msg_warning_selection(qtbot):
     assert msg_box.windowTitle() == name + " not selected"
     assert msg_box.text() == msg + action
     assert msg_box.standardButtons() == QMessageBox.StandardButton.Ok
+    datafile_close(datafile)
 
 
 def test_06_09_msg_warning_failed(qtbot):
-    dbref = Dbal()
-    main = QMainWindow()
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
-    qtbot.addWidget(main)
+    dialog, main, datafile = base_setup(qtbot)
     operation = "Add"
     msg = "The " + operation + " operation failed for some reason."
     msg_box = dialog.message_warning_failed(operation)
@@ -159,13 +133,11 @@ def test_06_09_msg_warning_failed(qtbot):
     assert msg_box.windowTitle() == operation + " Operation Failed"
     assert msg_box.text() == msg
     assert msg_box.standardButtons() == QMessageBox.StandardButton.Ok
+    datafile_close(datafile)
 
 
 def test_06_10_msg_warning_invalid(qtbot):
-    dbref = Dbal()
-    main = QMainWindow()
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
-    qtbot.addWidget(main)
+    dialog, main, datafile = base_setup(qtbot)
     error_msg = "3 Entries Missing"
     msg_box = dialog.message_warning_invalid()
     assert msg_box.icon() == QMessageBox.Icon.Warning
@@ -177,53 +149,50 @@ def test_06_10_msg_warning_invalid(qtbot):
     assert msg_box.windowTitle() == "Form Entries Invalid"
     assert msg_box.text() == "Please correct the highlighted errors\n" + error_msg
     assert msg_box.standardButtons() == QMessageBox.StandardButton.Ok
+    datafile_close(datafile)
 
 
 def test_06_11_action_cancel(qtbot, mocker):
-    dbref = Dbal()
-    main = QMainWindow()
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
-    qtbot.addWidget(main)
+    dialog, main, datafile = base_setup(qtbot)
     dialog.form = A_Form(dialog)
-    dialog.set_element(new_element(dbref, element_values))
+    dialog.set_element(new_element(datafile, element_values))
     # dialog is unchanged, just close
     assert dialog.action_cancel(save_something, 0)
 
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
+    dialog = Dialog(main, datafile, Dialog.VIEW_ELEMENT)
     qtbot.addWidget(main)
     dialog.form = A_Form(dialog)
-    dialog.set_element(new_element(dbref, element_values))
+    dialog.set_element(new_element(datafile, element_values))
     dialog.get_element().set_record_id(3456)
     # dialog is changed,
     mocker.patch.object(Dialog, "message_box_exec")
     dialog.message_box_exec.return_value = QMessageBox.StandardButton.No
     assert dialog.action_cancel(save_something, 0)
 
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
+    dialog = Dialog(main, datafile, Dialog.VIEW_ELEMENT)
     qtbot.addWidget(main)
     dialog.form = A_Form(dialog)
-    dialog.set_element(new_element(dbref, element_values))
+    dialog.set_element(new_element(datafile, element_values))
     dialog.get_element().set_record_id(3456)
     # dialog is changed,
     mocker.patch.object(Dialog, "message_box_exec")
     dialog.message_box_exec.return_value = QMessageBox.StandardButton.Yes
     assert dialog.action_cancel(save_something, 0)
 
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
+    dialog = Dialog(main, datafile, Dialog.VIEW_ELEMENT)
     qtbot.addWidget(main)
     dialog.form = A_Form(dialog)
-    dialog.set_element(new_element(dbref, element_values))
+    dialog.set_element(new_element(datafile, element_values))
     dialog.get_element().set_record_id(-5)
     # dialog is changed,
     mocker.patch.object(Dialog, "message_box_exec")
     dialog.message_box_exec.return_value = QMessageBox.StandardButton.Yes
     assert not dialog.action_cancel(save_something, 0)
+    datafile_close(datafile)
 
 
-def test_06_12_set_combo_box_selections(qtbot, db_create):
-    dbref = db_create
-    main = QMainWindow()
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
+def test_06_12_set_combo_box_selections(qtbot):
+    dialog, main, datafile = base_setup(qtbot)
     dialog.form = A_Form(dialog)
     ids = ["1", "3", "7", "10", "15", "60"]
     entry = "7"
@@ -236,16 +205,16 @@ def test_06_12_set_combo_box_selections(qtbot, db_create):
     assert dialog.form.record_id_combo.currentIndex() == -1
     assert dialog.form.record_id_combo.currentText() == ""
     assert dialog.form.record_id_combo.count() == len(ids)
+    datafile_close(datafile)
 
 
 def test_06_13_get_set_operation(qtbot):
-    dbref = Dbal()
-    main = QMainWindow()
-    dialog = Dialog(main, dbref, Dialog.VIEW_ELEMENT)
+    dialog, main, datafile = base_setup(qtbot)
     assert dialog.get_operation() == Dialog.VIEW_ELEMENT
     dialog.set_operation(Dialog.ADD_ELEMENT)
     assert dialog.get_operation() == Dialog.ADD_ELEMENT
-    dialog = Dialog(main, dbref, Dialog.EDIT_ELEMENT)
+    dialog = Dialog(main, datafile, Dialog.EDIT_ELEMENT)
     assert dialog.get_operation() == Dialog.EDIT_ELEMENT
     dialog.set_operation(10)
     assert dialog.get_operation() == Dialog.VIEW_ELEMENT
+    datafile_close(datafile)
