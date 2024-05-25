7c258130dac9c9b4137eb0b8fd70b412750cc2f1
LornBKerr <lornburtkerr@gmail.com>
2024-05-25 13:04:18 -0400

    Updated imports, revised test numbering.

--- tests/test_06_error_frame.py
+++ tests/test_06_error_frame.py
@@ -1,11 +1,18 @@
 """
 Test the ErrorFrame class.
 
-File:       test_07_combo_frame.py
+File:       test_06_error_frame.py
 Author:     Lorn B Kerr
 Copyright:  (c) 2023 Lorn B Kerr
 License:    MIT, see file License
 """
+
+import os
+import sys
+
+src_path = os.path.join(os.path.realpath("."), "src")
+if src_path not in sys.path:
+    sys.path.append(src_path)
 
 from PyQt5.QtWidgets import QFrame
 from pytestqt import qtbot
@@ -13,14 +20,14 @@ from pytestqt import qtbot
 from lbk_library.gui.error_frame import ErrorFrame
 
 
-def test_01_class_type(qtbot):
+def test_06_01_class_type(qtbot):
     frame = ErrorFrame()
     qtbot.addWidget(frame)
     assert isinstance(frame, ErrorFrame)
     assert isinstance(frame, QFrame)
 
 
-def test_02_error_property(qtbot):
+def test_06_02_error_property(qtbot):
     frame = ErrorFrame()
     qtbot.addWidget(frame)
     assert not frame._error
