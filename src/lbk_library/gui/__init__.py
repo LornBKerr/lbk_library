02ea1f60ebbba9dfc1deaa2eb64d5b200b708aae
LornBKerr <lornburtkerr@gmail.com>
2024-05-25 12:51:52 -0400

    Updated the comments.

--- src/lbk_library/gui/__init__.py
+++ src/lbk_library/gui/__init__.py
@@ -1,9 +1,10 @@
 """
-The Project Library Collection.
+The Project Library Collection Gui Package.
 
-This contains a set of common classes that support several projects.
+This package extendes the basic some Qt standard classes adding specific
+actions or combining seversl classes.
 
-This package contains the following classes:
+This package contains the following modules:
     ComboBox extends QComboBox - Emits the 'activate' signal when
         the focus is lost.
     Dialog extends QDialog - Base class for various dialogs used
@@ -30,8 +31,10 @@ from .combo_box import ComboBox
 from .dialog import Dialog
 from .error_frame import ErrorFrame
 from .line_edit import LineEdit
-from .row_state import RowState
-from .table_button_group import TableButtonGroup
-from .table_combo_box import TableComboBox
-from .table_line_edit import TableLineEdit
-from .table_push_button import TablePushButton
+from .table_widget_int_item import TableWidgetIntItem
+
+# from .row_state import RowState
+# from .table_button_group import TableButtonGroup
+# from .table_combo_box import TableComboBox
+# from .table_line_edit import TableLineEdit
+# from .table_push_button import TablePushButton
