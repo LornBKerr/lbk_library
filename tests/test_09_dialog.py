5301053c664179c2f2dfd192efd9a5b5ee50a7ac
LornBKerr <lornburtkerr@gmail.com>
2024-12-03 12:20:38 -0500

    Changed library PyQt6 to PySide6.

--- tests/test_09_dialog.py
+++ tests/test_09_dialog.py
@@ -14,8 +14,8 @@ src_path = os.path.join(os.path.realpath("."), "src")
 if src_path not in sys.path:
     sys.path.append(src_path)
 
-from PyQt5 import uic
-from PyQt5.QtWidgets import QDialog, QMainWindow, QMessageBox
+# from PySide6 import uic
+from PySide6.QtWidgets import QDialog, QMainWindow, QMessageBox
 from test_setup import (
     DummyForm,
     better_element,
