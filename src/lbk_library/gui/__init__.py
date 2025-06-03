f48d7c28dc118731b64fbd70ee79e403beba2aae
LornBKerr <lornburtkerr@gmail.com>
2025-06-03 11:04:13 -0400

    Added TableModel and Settings to the set of classes.

--- src/lbk_library/gui/__init__.py
+++ src/lbk_library/gui/__init__.py
@@ -15,20 +15,25 @@ This package contains the following modules:
         member to indicate an error.
     LineEdit extends QLineEdit - Emits the 'editingFinished' signal when
         the focus is lost.
-    TableWidgetIntItem extends QTableWidgetItem - Adds the capability of
+    Settings extends QSettings -Adds functionality to set and retrieve 
+        Lists as one-line calls.
+    TableModel extends QAbstractTableModel - Provides access to 
+        QTableView derived tables.
+    tableWidgetIntItem extends QTableWidgetItem - Adds the capability of
         sorting a table on an column of integers.
 
 File:       __init__.py
 Author:     Lorn B Kerr
-Copyright:  (c) 2023 Lorn B Kerr
+Copyright:  (c) 2023, 2025 Lorn B Kerr
 License:    MIT, see file LICENSE
-Version:    1.0.1
+Version:    1.1.0
 """
 
 from .combo_box import ComboBox
 from .dialog import Dialog
 from .error_frame import ErrorFrame
 from .line_edit import LineEdit
+from .settings import Settings
 from .table_model import CellData, TableModel
 from .table_widget_int_item import TableWidgetIntItem
 
@@ -36,4 +41,5 @@ file_version = "1.0.1"
 changes = {
     "1.0.0": "Initial release",
     "1.0.1": "Added Version Info",
+    "1.1.0": "Added TableModel and Settings to the set of classes."
 }
