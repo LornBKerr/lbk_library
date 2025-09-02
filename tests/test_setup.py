78939859471d63a4166134e0fc9f5739ccb52755
LornBKerr <lornburtkerr@gmail.com>
2025-09-02 11:19:09 -0400

    Remove unused, comment out statements.

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -26,12 +26,11 @@ if src_path not in sys.path:
 
 from PySide6.QtWidgets import QPushButton
 
-from lbk_library import DataFile, Element, ElementSet
+from lbk_library import Element  # DataFile, , ElementSet
 from lbk_library.gui import ComboBox, ErrorFrame, LineEdit
 
 datafile_name = "test_file.data"
 datafile_table = "elements"
-
 
 datafile_definition = [
     (
@@ -40,13 +39,6 @@ datafile_definition = [
         ' "installed" BOOLEAN,'
         ' "remarks" TEXT DEFAULT NULL)'
     ),
-]
-
-column_names = ["record_id", "name", "species", "tank_number", "remarks"]
-
-fish_value_set = [
-    [1, "Sammy", "shark", 1, "remark 1"],
-    [2, "Jamie", "cuttlefish", 7, "remark 2"],
 ]
 
 # set element values from array of values
@@ -63,15 +55,6 @@ element_definition = [
         ' "remarks" TEXT DEFAULT NULL)'
     ),
 ]
-element_record = {
-    "record_id": 1,
-    "remarks": "remark 1",
-}
-
-element_values = {
-    "record_id": 9876,
-    "remarks": "test",
-}
 
 
 def new_element(datafile, properties={}):
@@ -123,89 +106,3 @@ class DummyForm:
         self.line_edit = LineEdit(self.edit_error_frame)
         self.line_edit.setObjectName("line_edit")
         self.line_edit.setToolTip("Required: Enter the info, 1 to 15 Characters")
-
-
-class Fish(Element):
-    def __init__(self, datafile: DataFile, table_name: str, record_id: int = 0) -> None:
-        super().__init__(datafile, table_name)
-        properties = self.get_properties_from_datafile("record_id", record_id)
-        self.set_properties(properties)
-        self.set_initial_values(self.get_properties())
-        self.clear_value_changed_flags()
-
-    def set_properties(self, properties: dict[str, Any]) -> None:
-        if properties is not None and isinstance(properties, dict):
-            # Handle the 'record_id' and 'remarks' entries
-            results = super().set_properties(properties)
-            # Handle all the other properties here
-            for key in properties.keys():
-                if key == "name":
-                    results[key] = self.set_name(properties[key])
-                if key == "species":
-                    results[key] = self.set_species(properties[key])
-                if key == "tank_number":
-                    results[key] = self.set_tank_number(properties[key])
-        return results
-
-    def get_name(self) -> str:
-        name = self._get_property("name")
-        if name is None:
-            name = ""
-        return name
-
-    def set_name(self, name: str) -> dict[str, Any]:
-        result = self.validate.text_field(name, self.validate.REQUIRED, 1, 10)
-        if result["valid"]:
-            self._set_property("name", result["entry"])
-        else:
-            self._set_property("name", "")
-        return result
-
-    def get_species(self) -> str:
-        type = self._get_property("species")
-        if species is None:
-            species = ""
-        return type
-
-    def set_species(self, species: str) -> dict[str, Any]:
-        result = self.validate.text_field(species, self.validate.REQUIRED, 1, 10)
-        if result["valid"]:
-            self._set_property("species", result["entry"])
-        else:
-            self._set_property("species", "")
-        return result
-
-    def get_tank_number(self) -> str:
-        type = self._get_property("tank_number")
-        if tank_number is None:
-            tank_number = 0
-        return type
-
-    def set_tank_number(self, tank_number: int) -> dict[str, Any]:
-        result = self.validate.integer_field(tank_number, self.validate.REQUIRED, 1, 10)
-        if result["valid"]:
-            self._set_property("tank_number", result["entry"])
-        else:
-            self._set_property("tank_number", 0)
-        return result
-
-
-class FishSet(ElementSet):
-    def __init__(
-        self,
-        datafile,
-        table_name,
-        where_column: str = None,
-        where_value: str | int = None,
-        order_by_column: str = None,
-    ):
-        table_name = "fish"
-        element_type = Fish
-        super().__init__(
-            datafile,
-            table_name,
-            element_type,
-            where_column,
-            where_value,
-            order_by_column,
-        )
